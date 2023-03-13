8f43ee7033461782ca93ef07fcf795acd97eec3d
rafaelst2000 <rafaelst2000@gmail.com>
2023-03-13 15:47:07 -0300

    feat: init repository

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>teste</div>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  'brand-blue': '#3294f8',
+  'base-span': '#7b96b2',
+  'base-text': '#afc2d4',
+  'base-title': '#e7edf4',
+  'base-post': '#112131',
+  'base-label': '#3a536b',
+  'base-subtitle': '#c4d4e3',
+  'base-profile': '#0b1b2b',
+  'base-background': '#071422',
+  'base-border': '#1c2f41',
+  'base-input': '#040f1a',
+} as const
